d8ec30b3f449e95a7d37449528af36c13f2885ac
Vineet Agarwal <vineetagarwal2402@gmail.com>
2023-09-04 15:36:36 -0700

    added favorites

--- src/app/components/favorites/favorites.component.ts
+++ src/app/components/favorites/favorites.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { StoredFavorties } from 'src/app/interfaces/favorites.interface';
+import { StorageService } from 'src/app/services/storage.service';
 
 @Component({
   selector: 'app-favorites',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./favorites.component.css']
 })
 export class FavoritesComponent {
+
+  get favoriteEvents(): StoredFavorties {
+    return this.storageService.favoriteEvents;
+  }
+
+  favoriteEventsKeys(): string[] {
+    return Object.keys(this.favoriteEvents);
+  }
+
+  constructor(private storageService: StorageService) { }
+
+  removeFavorite(eventId: string) {
+    this.storageService.removeFromFavorites(eventId);
+  }
 
 }
